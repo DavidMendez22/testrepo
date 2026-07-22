2e30ca118c5005ea01421bdbce90c4cf83d70410
henry ramirez <david22mend@gmail.com>
2026-07-21 22:07:48 -0600

    new exercise

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,7 +17,15 @@ const checkCountValue = () => {
             alert('¡Tu publicación de Instagram ganó 10 seguidores! ¡Felicidades!')
         } else if (count === 20) {
             alert('Tu publicación de Instagram ganó 20 seguidores! ¡Sigue así!')
+        }else if(count === 0){
+            alert('El conteo de seguidores ha sido restablecido.')
         }
 
-    },500)
+    }, 800)
+}
+
+const resetCounter = () => {
+    count = 0
+    displayCount()
+    checkCountValue()
 }
